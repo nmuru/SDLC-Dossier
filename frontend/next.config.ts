d3117e35cc3d5860b25735325b7d2d8cbba5495d
NM <nmurugs@gmail.com>
2026-09-20 13:55:29 +0100

    next.config.ts added cross origin

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const Production_mode = true;
 
 const nextConfig: NextConfig = {
+  allowedDevOrigins: ['3.87.44.100'],
   env: {
     NEXT_PUBLIC_API_URL: Production_mode
       ? "https://sdlc-dossier-api.onrender.com"
