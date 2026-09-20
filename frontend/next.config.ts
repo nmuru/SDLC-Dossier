7d051b198e946e02deed5fe627b4bdaf74784ad6
NM <nmurugs@gmail.com>
2026-09-20 14:10:22 +0100

    server name in next config changed

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,13 +1,9 @@
-import type { NextConfig } from "next";
-
 const Production_mode = true;
 
 const nextConfig: NextConfig = {
   allowedDevOrigins: ['3.87.44.100'],
   env: {
-    NEXT_PUBLIC_API_URL: Production_mode
-      ? "https://sdlc-dossier-api.onrender.com"
-      : "http://localhost:8000",
+    NEXT_PUBLIC_API_URL: "http://3.87.44.100:8000",
   },
 };
 
