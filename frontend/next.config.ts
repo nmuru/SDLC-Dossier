2996ddf80b4565391c411fe67b22b2940cd71610
NM <nmurugs@gmail.com>
2026-09-20 15:07:59 +0100

    allowed origin in config py modified

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,6 +1,7 @@
 const Production_mode = true;
 
-const nextConfig: NextConfig = {
+const nextConfig:
+ NextConfig = {
   allowedDevOrigins: ['3.87.44.100'],
   env: {
     NEXT_PUBLIC_API_URL: "http://3.87.44.100:8000",
