66c1e585628cd98e1c6a5cee11d8cf415cb333b8
NM <nmurugs@gmail.com>
2026-09-21 05:24:11 +0100

    next config corrected for https

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,11 +1,11 @@
 import type { NextConfig } from "next";
 
-const Production_mode = false;
+const Production_mode = true;
 
 const nextConfig: NextConfig = {
   env: {
     NEXT_PUBLIC_API_URL: Production_mode
-      ? "https://sdlc-dossier-api.onrender.com"
+      ? "http://34.205.18.52:8000"
       : "http://localhost:8000",
   },
 };
