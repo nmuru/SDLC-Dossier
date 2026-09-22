513480adaa82a117ee27b8954009d4e913543685
nmuru <47250209+nmuru@users.noreply.github.com>
2026-09-22 16:24:04 +0530

    Update API URL for production environment

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -5,7 +5,7 @@ const Production_mode = true;
 const nextConfig: NextConfig = {
   env: {
     NEXT_PUBLIC_API_URL: Production_mode
-      ? "https://sdlc-dossier-api.onrender.com"
+      ? "https://3.91.148.139"
       : "http://localhost:8000",
   },
 };
